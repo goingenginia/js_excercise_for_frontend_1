088ef2e6023377fb767193f41281e21da25114d8
Iwanaga <iwanaga@IwanaganoMacBook-Air.local>
2019-09-09 22:08:05 +0900

    課題に取り組みました。

--- main.js
+++ main.js
@@ -3,31 +3,31 @@
  *   - getElementByIdを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+console.log(document.getElementById("title"));
 
 /**
  * 課題2: id値が 'item-container' の要素を取得する
  *   - querySelectorを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+console.log(document.querySelector("ul"));
 
 /**
  * 課題3: li要素を全て取得する
  *   - console.logで取得したDOMの中身を確認する
  */
-
+console.log(document.getElementsByTagName("li"));
 
 /**
  * 課題4: class値が 'item' の要素を全て取得する
  *   - getElementsByClassNameを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+console.log(document.getElementsByClassName("item"));
 
 /**
  * 課題5: class値が 'item' の要素を全て取得する
  *   - querySelectorAllを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-
+console.log(document.querySelectorAll(".item"));
