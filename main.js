102aacd0e7d27c9315d0248eed3b5d37b6946836
Iwanaga <iwanaga@IwanaganoMacBook-Air.local>
2019-09-11 20:27:57 +0900

    コメント分修正しました

--- main.js
+++ main.js
@@ -3,31 +3,36 @@
  *   - getElementByIdを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-console.log(document.getElementById("title"));
+const title = document.getElementById("title")
+console.log(title);
 
 /**
  * 課題2: id値が 'item-container' の要素を取得する
  *   - querySelectorを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-console.log(document.querySelector("ul"));
+const itemcontainer = document.querySelector("#item-container")
+console.log(itemcontainer);
 
 /**
  * 課題3: li要素を全て取得する
  *   - console.logで取得したDOMの中身を確認する
  */
-console.log(document.getElementsByTagName("li"));
+const li = document.getElementsByTagName("li")
+console.log(li);
 
 /**
  * 課題4: class値が 'item' の要素を全て取得する
  *   - getElementsByClassNameを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-console.log(document.getElementsByClassName("item"));
+const firstitem = document.getElementsByClassName("item")
+console.log(firstitem);
 
 /**
  * 課題5: class値が 'item' の要素を全て取得する
  *   - querySelectorAllを使う
  *   - console.logで取得したDOMの中身を確認する
  */
-console.log(document.querySelectorAll(".item"));
+const seconditem = document.querySelectorAll(".item")
+console.log(seconditem);
